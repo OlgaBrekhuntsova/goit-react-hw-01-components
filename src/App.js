2255b712f619cfd5d611582a782d477d0b49f9b2
Olga <olga.pistryak@gmail.com>
2021-04-17 22:42:37 +0300

    Me first react commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from "./components/Card";
+const App = () => {
+    return (
+        <div>1st react string
+         <Card/>
+        </div>
+        
+    );
+};
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Card = () => { 
+    return (<p>Card</p>);
+};
+export default Card;
